9954067df1d34ca9f7a57d6c9095be2c3306c52c
Gernot Roetzer <gernot.roetzer@gmail.com>
2017-06-28 20:51:46 +0100

    First working version.

--- cmaes/cmaes.py
+++ cmaes/cmaes.py
@@ -2,22 +2,25 @@ from operator import itemgetter
 import sys
 
 import numpy as np
+import scipy.linalg as spla
 import scipy.special as spsp
+
+np.random.seed(5)
 
 class CovMatAdapt:
 
     def __init__(self, func, mean_vec, step_size, pop_size=None,
                      elite_size=None, weights=None,
                      c_sig=None, d_sig=None, c_c=None,
-                     c_1=None, c_mu=None, c_m=None, abs_tol=-1e3,
-                     rel_tol=1e-4, maxiter=50):
+                     c_1=None, c_mu=None, c_m=None, abs_tol=1e-5,
+                     rel_tol=1e-4, maxgen=50):
 
         self.func = func
         self.mean_vec = mean_vec
         self.step_size = step_size
         self.abs_tol = abs_tol
         self.rel_tol = rel_tol
-        self.maxiter = maxiter
+        self.maxgen = maxgen
         self.ndim = mean_vec.size
 
         # Default values for population sizes and weights
@@ -112,36 +115,105 @@ class CovMatAdapt:
         conc_matrix = conc_matrix[conc_matrix[:,0].argsort()]
         return conc_matrix
 
-    def zip_and_sort(self, func_values, x, y):
-        zipped = list(zip(func_values, x, y))
-        zipped.sort(key=itemgetter(0))
-        return zipped
+    def heaviside(self, gen, norm_p_sig, c_sig, ndim, expec_norm_gaussian):
+        a = norm_p_sig / (np.sqrt(1 - (1 - c_sig)**(2*(gen + 1))))
+        b = (1.4 + 2/(ndim + 1)) * expec_norm_gaussian
 
+        if a < b:
+            return 1
+        else:
+            return 0
 
     def minimize(self):
 
         expec_norm_gaussian = (np.sqrt(2) * spsp.gamma(0.5*(self.ndim + 1)) /
                                spsp.gamma(0.5*self.ndim))
-        y, x, func_values = self.sample_and_evaluate(self.func, self.ndim,
+        pop_matrix = self.sample_and_evaluate(self.func, self.ndim,
                                                 self.mean_vec, self.cov_mat,
                                                 self.pop_size, self.step_size)
-        sorted_values = self.zip_and_sort(func_values, x, y)
 
-        y = sorted_values[2]
-        x = sorted_values[1]
-        func_values = sorted_values[0]
-        print(y, x, func_values, sep='\n')
-        print(type(y))
-        best_fvalue = sorted_values[0][0]
-        best_param = sorted_values[0][1]
+        y = pop_matrix[:,(self.ndim+1):]
+        x = pop_matrix[:,1:(self.ndim+1)]
+        func_values = pop_matrix[:,0]
+        best_fvalue = pop_matrix[0,0]
+        best_param = x[0,:]
 
-        for i in range(self.maxiter):
+        for gen in range(1, self.maxgen + 1):
             y_weighted = np.sum(y[:self.elite_size] *
-                             self.weights[:self.elite_size])
-            self.mean_vec = self.mean_vec + self.c_m*self.step_size*y_weighted
-            return None
+                             self.weights[:self.elite_size, np.newaxis], axis=0)
+            upd_mean_vec = self.mean_vec + self.c_m*self.step_size*y_weighted
+            diff_vec = abs(upd_mean_vec - self.mean_vec)
+
+            if (max(diff_vec) < self.abs_tol or
+                max(diff_vec/abs(self.mean_vec)) < self.rel_tol):
+                result_dict = {'best fvalue': best_fvalue,
+                               'best param' : best_param,
+                               'mean vector': self.mean_vec,
+                               'cov matrix': self.cov_mat,
+                               'converged in': gen,
+                               'abs/rel conv': (max(diff_vec),
+                                                max(diff_vec/abs(self.mean_vec)))}
+                return result_dict
+
+            self.mean_vec = upd_mean_vec
+            chol_covm_L = spla.cholesky(self.cov_mat, lower=True)
+
+            c_y_vec = spla.solve_triangular(chol_covm_L, y_weighted, lower=True)
+
+            # Updating the step sizes
+            self.p_sig = ((1 - self.c_mu)*self.p_sig +
+                         np.sqrt(self.c_sig*(2 - self.c_sig)*self.mu_eff) *
+                         c_y_vec)
+            norm_p_sig = np.linalg.norm(self.p_sig)
+            self.step_size = (self.step_size *
+                             np.exp(self.c_sig / self.d_sig *
+                                    norm_p_sig /
+                                    expec_norm_gaussian - 1))
+
+            # Updating the covariance matrix
+            h_sig = self.heaviside(gen, norm_p_sig, self.c_sig,
+                              self.ndim, expec_norm_gaussian)
+
+            self.p_c = ((1 - self.c_c)*self.p_c +
+                   h_sig*np.sqrt(self.c_c*(2 - self.c_c)*self.mu_eff) *
+                   y_weighted)
+            c_y_mat = spla.solve_triangular(chol_covm_L,
+                                            y[self.elite_size:,:].T,
+                                            lower=True).T
+            c_y_mat_norm = np.linalg.norm(c_y_mat, axis=1)
+
+            w_adj = self.weights.copy()
+            w_adj[self.elite_size:] = (w_adj[self.elite_size:] *
+                                       (self.ndim / c_y_mat_norm))
+
+            matrix_sum = (w_adj*y.T).dot(y)
+            self.cov_mat = ((1 + self.c_1*(1 - h_sig)*self.c_c*(2 - self.c_c) -
+                            self.c_1 - self.c_mu*sum(self.weights)) *
+                            self.cov_mat + self.c_1*np.outer(self.p_c, self.p_c)
+                            + self.c_mu*matrix_sum)
+
+            # Create new generations
+            pop_matrix = self.sample_and_evaluate(self.func, self.ndim,
+                                                self.mean_vec, self.cov_mat,
+                                                self.pop_size, self.step_size)
+
+            y = pop_matrix[:,(self.ndim+1):]
+            x = pop_matrix[:,1:(self.ndim+1)]
+            func_values = pop_matrix[:,0]
+
+            if func_values[0] < best_fvalue:
+                best_fvalue = pop_matrix[0,0]
+                best_param = x[0,:]
+
         else:
-            print('Failed to converge within maximum iteration bound.')
+            print('Failed to converge within maximum generation bound.')
+            result_dict = {'best fvalue': best_fvalue,
+                           'best param' : best_param,
+                           'mean vector': self.mean_vec,
+                           'cov matrix': self.cov_mat,
+                           'converged in': self.maxgen}
+            return result_dict
+
 
 def test_func(vec):
     x, y = vec
@@ -150,6 +222,6 @@ def test_func(vec):
 if __name__ == '__main__':
     mean_vec = np.array([1, 2])
 
-    cma_obj = CovMatAdapt(test_func, mean_vec, step_size=1, pop_size=9)
+    cma_obj = CovMatAdapt(test_func, mean_vec, step_size=1, pop_size=25)
 
     print(cma_obj.minimize())
